dcbe2625a05fba73fd3afe6b411baaee74a4f192
wabualela <wailabualela@gmail.com>
2024-04-28 10:37:28 +0300

    Day 5: Refactor layout component and nav-link component in Laravel app

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
 
 <head>
     <meta charset="utf-8">
@@ -9,7 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
+<body class="h-full">
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -21,14 +21,10 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                                    aria-current="page">Home</a>
-                                <a href="/about"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                                <a href="/contact"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-                            </div>
+                                    <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                    <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                                    <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                                </div>
                         </div>
                     </div>
                     <div class="hidden md:block">
